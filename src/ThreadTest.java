8b506bdcb8267ac1826737390eab1f082acb6da7
Amirmohseni87 <amirmohseni87@yahoo.com>
2023-10-12 18:42:32 +0200

    Fixed and new code

--- src/ThreadTest.java
+++ src/ThreadTest.java
@@ -1,3 +1,4 @@
+
 public class ThreadTest extends Thread {
     public static long runTime;
     public void run() {
@@ -22,13 +23,18 @@ public class ThreadTest extends Thread {
          */
         long total = 0;
         int iterNum = 100;
+        int minTime = 100000, maxTime = 0;
         for (int i = 0; i < iterNum; i++) {
             long startTime = System.currentTimeMillis();
             Search.main(null);
             long endTime = System.currentTimeMillis();
             total += endTime - startTime;
+            minTime = Math.min(minTime, (int)(endTime - startTime));
+            maxTime = Math.max(maxTime, (int)(endTime - startTime));
         }
+        System.out.println("Min time: " + minTime);
         System.out.println("Average time: " + total/iterNum);
+        System.out.println("Max time: " + maxTime);
     }
 
 }
