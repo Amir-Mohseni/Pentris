459825d68f6349ead8aad541d30e8e6fcaa353ee
KoungMyatHein-UM <k.koungmyathein@student.maastrichtuniversity.nl>
2023-11-21 10:50:27 +0100

    emptyFullRows2 now returns the Added chunk assignment

--- src/Board.java
+++ src/Board.java
@@ -218,7 +218,51 @@ public class Board {
         return totalFullRows;
     }
 
+    public void gravityChunks(){
+        int groups = 0;
+        List<Integer> groupIDs = new ArrayList<>();
 
+        int[][] chunkGroups = new int[this.HEIGHT][this.WIDTH];
+        for (int[] row : chunkGroups){
+            Arrays.fill(row, -1);
+        }
+
+        // iterate through the grid
+        for (int row = 0; row < this.grid.length; row++){
+            for (int col = 0; col < this.grid[row].length; col++){
+                if (this.grid[row][col] != -1 && chunkGroups[row][col] == -1){
+                    groupIDs.add(groups);
+                    chunkGroups = viral(row, col, groups++, chunkGroups);
+                }
+            }
+        }
+
+        for (int[] row : chunkGroups){
+            System.out.println(Arrays.toString(row));
+        }
+
+//        for (Integer id : groupIDs){
+//            for (int col = 0; col < this.)
+//        }
+    }
+
+    public int[][] viral(int row, int col, int val, int[][] chunkGroups){
+        chunkGroups[row][col] = val;
+        int[][] offsets = new int[][]{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        for (int[] offset : offsets){
+            try{
+                int offsetedValue = chunkGroups[row + offset[0]][col + offset[1]];
+                int offsetedGridValue = this.grid[row + offset[0]][col + offset[1]];
+
+                if (offsetedValue != val && offsetedGridValue != -1){
+                    viral(row + offset[0], col + offset[1], val, chunkGroups);
+                }
+            } catch (ArrayIndexOutOfBoundsException e){
+                continue;
+            }
+        }
+        return chunkGroups;
+    }
 
     public boolean arrayContainsFull(int[] arr){
         for (int i : arr){
@@ -248,9 +292,40 @@ public class Board {
     }
 
     public static void main(String[] args) {
-        Board board = new Board(5,15);
-//        board.grid[0] = new int[]{-1, -1, 4, 4, -1};
-        int[] row = new int[]{4, 4, 4 ,4, 4};
-        System.out.println(board.arrayContainsFull(row));
+        Board board = new Board(5,18);
+        board.grid = new int[][]{
+                {-1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1},
+                {-1, -1, -1,  0, -1},
+                { 0,  0,  0,  0, -1},
+                { 0,  0,  0,  0, -1},
+                {-1,  0,  0,  0, -1},
+                {-1, -1, -1,  0,  0},
+                { 0,  0,  0, -1, -1},
+                {-1,  0, -1, -1, -1},
+                { 0,  0, -1, -1, -1},
+        };
+
+        board.gravityChunks();
+
+//        int[][] chunkGroups = new int[board.HEIGHT][board.WIDTH];
+//        for (int[] row : chunkGroups){
+//            Arrays.fill(row, -1);
+//        }
+//
+////        board.groupChunks();
+//        int[][] resultChunks = board.viral(10, 3, 9, chunkGroups);
+//
+//        for (int[] row : resultChunks){
+//            System.out.println(Arrays.toString(row));
+//        }
     }
 }
